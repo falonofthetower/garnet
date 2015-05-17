4f4332e983c6aa3b435f8df8f6a179774dea7107
Peter Karth <falonofthetower@gmail.com>
2015-05-17 16:54:54 -0400

    behold user creation!

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,17 +11,30 @@ describe UsersController do
   describe "POST create" do
    context "with valid attributes" do
      before do
-       post :create, user: Fabricate(:user)
+       post :create, user: Fabricate.attributes_for(:user)
      end
+
      it "saves the user in the database" do
        expect(User.count).to eq(1)
      end
-     it "redirects to the index page"
+
+     it "redirects to the index page" do
+       expect(response).to redirect_to(trials_path)
+     end
    end
 
    context "with invalid attributes" do
-     it "does not save the user in the database"
-     it "re-renders the :new tamplate"
+     before do
+       post :create, user: { email: "neo@example.com" }
+     end
+
+     it "does not save the user in the database" do
+       expect(User.count).to eq(0)
+     end
+
+     it "re-renders the :new tamplate" do
+       expect(response).to render_template(:new)
+     end
    end
   end
 end
