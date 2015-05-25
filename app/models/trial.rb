e42b81ef9b3750fc75e0d8cfdde2ae6ba272b957
Peter Karth <falonofthetower@gmail.com>
2015-05-25 16:54:03 -0400

    corrected all_expired

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -9,14 +9,6 @@ class Trial < ActiveRecord::Base
   end
 
   def self.all_expired(current_user)
-    where(user_id: current_user.id).where("expiration_date < ?", Time.zone.today).order("expiration_date ASC")
-  end
-
-  def expired
-
-  end
-
-  def active
-
+    where(user_id: current_user.id).where("expiration_date <= ?", Time.zone.today).order("expiration_date ASC")
   end
 end
