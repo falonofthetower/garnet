94fc0edaf50c6a1fc4608498723e91124327556f
Peter Karth <falonofthetower@gmail.com>
2015-05-25 11:52:42 -0400

    tweaked login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      flash[:success] = "You are now an Untrialer!"
       redirect_to trials_path
     else
       render :new
